d63ac4936769836eb4c147045745b09451a3e69f
Antonio <antonio.sanna2706@gmail.com>
2021-10-21 17:09:31 +0200

    push Django

--- index.php
+++ index.php
@@ -20,3 +20,23 @@
     $django = new Movie ('Django', 'Quentin Tarantino', 2013,'Epico');
     $venom = new Movie ('Venom', 'Ruben Fleischer', 2018, 'fantascienza');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 1</title>
+</head>
+<body>
+    <div class="movie">
+        <h1>
+            <?php
+                echo $django->name;
+            ?>
+        </h1>
+    </div>
+    
+</body>
+</html>
